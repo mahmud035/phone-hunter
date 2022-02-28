3447540e374a59b2bd069696a29f090b412edeb6
mahmud035 <mahamudulhasan4148@gmail.com>
2022-02-28 22:27:36 +0600

    add search area

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 'use strict';
 
+const loadPhones = () => {
+  const input = document.getElementById('search-box');
+  const searchText = input.value;
+  input.value = '';
 
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayPhones(data.data));
+};
+
+const displayPhones = (phones) => {
+  console.log(phones);
+};
