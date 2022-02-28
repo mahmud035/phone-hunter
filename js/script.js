d706e155e75ff8bd5c8bb8e054a0c7f820f105d2
mahmud035 <mahamudulhasan4148@gmail.com>
2022-03-01 00:59:13 +0600

    others information & optional chaining

--- js/script.js
+++ js/script.js
@@ -54,38 +54,43 @@ const loadSinglePhone = (phoneId) => {
 
 const displayPhoneDetails = (phone) => {
   const phoneDetail = document.getElementById('phone-detail');
+  phoneDetail.textContent = '';
   console.log(phone);
 
   const div = document.createElement('div');
   div.innerHTML = `
-      <div class="card  border-0">
-        <img src="${phone.image}" class="card-img-top w-50 h-50 mx-auto pt-3">
-        <div class="card-body">
+    <div class="card  border-0">
+       <img src="${phone.image}" class="card-img-top w-50 h-50 mx-auto pt-3">
+       <div class="card-body">
           <h3 class="text-center">${phone.name}</h3>
-          <h6 class="text-center">${phone.releaseDate}</h6>
+          <h6 class="text-center">${phone?.releaseDate}</h6>
           <h3 class="text-center">${phone.brand}</h3>
           <hr>
-          <h5 class="text-center">Main Features:</h5>
-          <p>ChipSet: ${phone.mainFeatures.chipSet} </p> 
-          <p> Display Size: ${phone.mainFeatures.displaySize} </p>
-          <p>Memory: ${phone.mainFeatures.memory} </p>
-          <p> Storage: ${phone.mainFeatures.storage} </p>
+          <h5 class="text-center">Main Features</h5>
+          <p> ChipSet: ${phone?.mainFeatures?.chipSet} </p> 
+          <p> Display Size: ${phone?.mainFeatures?.displaySize} </p>
+          <p> Memory: ${phone?.mainFeatures?.memory} </p>
+          <p> Storage: ${phone?.mainFeatures?.storage} </p>
           <hr>
-          <h5 class="text-center">Sensors:</h5>
-          <p>${phone.mainFeatures.sensors[0]},
-             ${phone.mainFeatures.sensors[1]}, 
-             ${phone.mainFeatures.sensors[2]}, 
-             ${phone.mainFeatures.sensors[3]}, 
-             ${phone.mainFeatures.sensors[4]},  
-             ${phone.mainFeatures.sensors[5]}
+          <h5 class="text-center">Sensors</h5>
+          <p>${phone?.mainFeatures?.sensors[0]},
+             ${phone?.mainFeatures?.sensors[1]}, 
+             ${phone?.mainFeatures?.sensors[2]}, 
+             ${phone?.mainFeatures?.sensors[3]}, 
+             ${phone?.mainFeatures?.sensors[4]},  
+             ${phone?.mainFeatures?.sensors[5]}
           </p> 
           <hr>
-          
-     
-        </div>
+          <h5 class="text-center">Others</h5>
+          <p> Bluetooth: ${phone?.others?.Bluetooth} </p> 
+          <p> GPS: ${phone?.others?.GPS} </p>
+          <p> NFC: ${phone?.others?.NFC} </p>
+          <p> Radio: ${phone?.others?.Radio} </p>
+          <p> USB: ${phone?.others?.USB} </p>
+          <p> WLAN: ${phone?.others?.WLAN} </p>
+          <hr>
       </div>
-  
-  `;
+    </div>`;
 
   phoneDetail.appendChild(div);
 };
