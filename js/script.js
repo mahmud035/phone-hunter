1bd4714fb6b908e567aa830cb2b3bf82f88aef11
mahmud035 <mahamudulhasan4148@gmail.com>
2022-02-28 23:06:17 +0600

    display phone on UI

--- js/script.js
+++ js/script.js
@@ -13,5 +13,23 @@ const loadPhones = () => {
 };
 
 const displayPhones = (phones) => {
-  console.log(phones);
+  const phoneContainer = document.getElementById('phone-container');
+
+  phones.forEach((phone) => {
+    // console.log(phone);
+    const div = document.createElement('div');
+    // div.className = 'p-1 m-1';
+
+    div.innerHTML = `
+      <div class="card text-center border-0">
+        <img src="${phone.image}" class="card-img-top w-50 h-50 mx-auto pt-3">
+        <div class="card-body">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <h5 class="card-title">${phone.brand}</h5>
+          <button class="btn btn-success">See Details</button>
+        </div>
+      </div>`;
+
+    phoneContainer.appendChild(div);
+  });
 };
